fc211e7488027a223abadf2ec831a597cf766ad2
Anirudh-Viswanatha <148857606+Anirudh-Viswanatha@users.noreply.github.com>
2025-05-20 18:38:22 -0700

    Create question_model.py

---
+++ Day17_of_100DaysOfCode_Quiz_game_project/question_model.py
@@ -0,0 +1,5 @@
+class Question:
+    def __init__(self, q_text, q_answer):
+        self.text = q_text       # attribute 1
+        self.answer = q_answer    # attribute 2
+# these 2 attributes will get initialized when we create a new object from "Question" class.
